dbe1ce50c2a90468ad1ccf9295a95d4f60e5f65e
imhilla <hillaryodhiambo282@gmail.com>
2020-09-16 08:07:43 +0300

    add nav items

--- src/nav.js
+++ src/nav.js
@@ -1,17 +1,27 @@
 const navModule = (() => {
   const navBar = () => {
-    const nav = document.createElement('div');
+    const nav = document.createElement('nav');
+    nav.setAttribute('class', 'nav-container')
     nav.setAttribute('id', 'nav-id');
     const leftDiv = document.createElement('div');
     leftDiv.setAttribute('id', 'left-container')
-    const elem = document.createElement('p');
-    const text = document.createTextNode("This just got added");
+    const elem = document.createElement('h1');
+    //elem.setAttribute('class', 'navitem')
+    const text = document.createTextNode("Todo list");
     const rightDiv = document.createElement('div')
     rightDiv.setAttribute('id', 'right-container')
+    const fastTask = document.createElement('div')
+    fastTask.innerHTML = `<a href="#" class='plus' id='plus'>+</a>`
+    //fastTask.setAttribute('class', 'fast-add')
+    const bell = document.createElement('div')
+    bell.setAttribute('class', 'bell')
+    bell.innerHTML = `<a href="#" class='bell-item'>notification</a></i>`
     nav.appendChild(leftDiv);
     nav.appendChild(rightDiv)
     leftDiv.appendChild(elem)
     elem.appendChild(text);
+    rightDiv.appendChild(fastTask)
+    rightDiv.appendChild(bell)
     return nav
   }
   document.querySelector('.nav').appendChild(navBar());
