ed582309c6f982485a4b89122e6e66a7dc2539e7
imhilla <hillaryodhiambo282@gmail.com>
2020-09-16 09:52:21 +0300

    add setting in nav

--- src/nav.js
+++ src/nav.js
@@ -6,22 +6,23 @@ const navModule = (() => {
     const leftDiv = document.createElement('div');
     leftDiv.setAttribute('id', 'left-container')
     const elem = document.createElement('h1');
-    //elem.setAttribute('class', 'navitem')
     const text = document.createTextNode("Todo list");
     const rightDiv = document.createElement('div')
     rightDiv.setAttribute('id', 'right-container')
     const fastTask = document.createElement('div')
     fastTask.innerHTML = `<a href="#" class='plus' id='plus'>+</a>`
-    //fastTask.setAttribute('class', 'fast-add')
     const bell = document.createElement('div')
     bell.setAttribute('class', 'bell')
     bell.innerHTML = `<a href="#" class='bell-item'>notification</a></i>`
+    const setting = document.createElement('div')
+    setting.innerHTML = `<a href="#" class='setting-item'>setting</a></i>`
     nav.appendChild(leftDiv);
     nav.appendChild(rightDiv)
     leftDiv.appendChild(elem)
     elem.appendChild(text);
     rightDiv.appendChild(fastTask)
     rightDiv.appendChild(bell)
+    rightDiv.appendChild(setting)
     return nav
   }
   document.querySelector('.nav').appendChild(navBar());
