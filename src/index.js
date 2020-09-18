7475b1d02e5a8eedc5f5d0a697eb374e4b501d0f
imhilla <hillaryodhiambo282@gmail.com>
2020-09-18 19:41:25 +0300

    pass local storage to delete function

--- src/index.js
+++ src/index.js
@@ -122,7 +122,9 @@ const executeOddClick = () => {
             projectTitleDivForTaskParentDiv.appendChild(taskList);
             document.getElementById('delete').addEventListener('click', () => {
               taskListValues.splice(idx, 1);
+              localStorage.setItem('todo', JSON.stringify(taskListValues));
               console.log(taskListValues);
+              console.log(localStorage)
             })
           }
         });
